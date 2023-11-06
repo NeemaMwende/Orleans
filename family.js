f0fd7ab26cb3fa19f446bccf76e9a549ae26d893
Neema_Mwende <gracepete12@gmail.com>
2023-11-06 16:44:08 +0300

    preload animation

---
+++ family.js
@@ -0,0 +1,4 @@
+// After your website content has fully loaded, add the "loaded" class to the body element
+document.addEventListener("DOMContentLoaded", function () {
+    document.body.classList.add("loaded");
+});
